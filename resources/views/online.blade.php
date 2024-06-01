3902f43210a368c4c7a12b5144357de7128296a4
harrylee02 <ngochieu0211.study@gmail.com>
2024-06-01 11:05:14 +0700

    edit

--- resources/views/online.blade.php
+++ resources/views/online.blade.php
@@ -9,17 +9,11 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3">
-                <div id="clock-countdown">
-                    <div id="clock"><span id="timer1">10:00</span></div>
-                </div>
                 <div class="center">
                     <br>
                     <div id="myBoard"style="width: 410px; margin: 0 auto;"></div>
                     <br>
                     <button id="resign" type="button" class="btn btn-danger">Resign</button>
-                </div>
-                <div id="clock-countdown2">
-                    <div id="clock"><span id="timer2">10:00</span></div>
                 </div>
             </div>
             <div class="col-md-5">
